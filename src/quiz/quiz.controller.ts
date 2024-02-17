9e7d60f37c0495154bc690a66f7681c35f89caa5
theanh <htheanh2000@gmail.com>
2024-02-17 15:30:27 +0700

    feat: add get quiz by id

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { Quiz } from './entities/quiz.entity';
+import { Question } from './entities/question.entity';
+import { Option } from './entities/option.entity';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @Controller('quizzes')
 export class QuizController {
@@ -9,5 +12,15 @@ export class QuizController {
   @Get()
   async findAll(): Promise<Quiz[]> {
     return this.quizService.findAll();
+  }
+  
+  @Get(':id')
+  @ApiOperation({ summary: 'Get a quiz by ID' })
+  @ApiResponse({ status: 200, description: 'The quiz details.' })
+  @ApiResponse({ status: 404, description: 'Quiz not found.' })
+  @ApiParam({ name: 'id', type: 'number' })
+  @Get(':id')
+  async getQuizById(@Param('id') id: number): Promise<Quiz> {
+    return this.quizService.findOne(id);
   }
 }
--- src/quiz/quiz.service.ts
+++ src/quiz/quiz.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@nestjs/common';
-// Assume QuizEntity is your entity related to quizzes
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Question } from './entities/question.entity';
 import { Quiz } from './entities/quiz.entity';
+import { Option } from './entities/option.entity';
 
 @Injectable()
 export class QuizService {
 
   async findAll(): Promise<Quiz[]> {
-    return null
+    const quiz = Quiz.findAll();
+    return quiz ;
   }
+
+  async findOne(quizId: number): Promise<Quiz> {
+    const quiz = await Quiz.findByPk(quizId, {
+        include: [{ model: Question, include: [Option] }],
+      });
+    
+      if (!quiz) {
+        throw new NotFoundException(`Quiz with ID ${quizId} not found`);
+      }
+    
+      return quiz;
+    }
 }
